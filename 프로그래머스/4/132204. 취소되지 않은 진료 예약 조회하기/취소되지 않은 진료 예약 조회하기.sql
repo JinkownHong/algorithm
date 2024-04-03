65ece278d3e39019aa868bdedd328025199f7c6f
JinkownHong <161419351+JinkownHong@users.noreply.github.com>
2024-04-03 16:12:47 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,21 +1,17 @@
-#PATIENT, DOCTOR 그리고 APPOINTMENT 테이블에서
-#2022년 4월 13일 취소되지 않은 흉부외과(CS) 진료 예약 내역을 조회
-#진료예약번호, 환자이름, 환자번호, 진료과코드, 의사이름, 진료예약일시 항목이 출력되도록 작성
-#결과는 진료예약일시를 기준으로 오름차순 정렬해주세요.
-SELECT APNT_NO,
-       PT_NAME,
-       J.PT_NO,
-       MCDP_CD,
-       DR_NAME,
-       APNT_YMD
-FROM
-(
-SELECT D.DR_NAME,
-        A.APNT_NO,
-        A.MCDP_CD,
-        A.APNT_YMD,
-        A.PT_NO
-FROM DOCTOR D JOIN APPOINTMENT A ON D.DR_ID=A.MDDR_ID
-WHERE D.MCDP_CD = 'CS' AND A.APNT_YMD LIKE '2022-04-13%' AND A.APNT_CNCL_YN = 'N'
-  ) J INNER JOIN PATIENT P ON J.PT_NO=P.PT_NO
-ORDER BY APNT_YMD
+WITH NOT_CANCEL AS (
+    SELECT A.APNT_YMD,
+           A.APNT_NO,
+           A.PT_NO,
+           A.MCDP_CD,
+           A.MDDR_ID,
+           D.DR_NAME
+    FROM APPOINTMENT A JOIN DOCTOR D ON A.MDDR_ID=D.DR_ID
+    WHERE 1=1 
+        AND
+        A.APNT_CNCL_YN = 'N'
+        AND
+        A.APNT_YMD LIKE '2022-04-13%')
+        
+SELECT NC.APNT_NO, P.PT_NAME, NC.PT_NO, NC.MCDP_CD, NC.DR_NAME, NC.APNT_YMD
+FROM NOT_CANCEL NC JOIN PATIENT P ON NC.PT_NO = P.PT_NO
+ORDER BY NC.APNT_YMD
