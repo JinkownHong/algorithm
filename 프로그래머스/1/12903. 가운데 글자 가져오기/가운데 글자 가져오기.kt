fca93eb10071b46bd0c5af383ab8e2605ec01f13
JinkownHong <161419351+JinkownHong@users.noreply.github.com>
2024-05-14 09:33:30 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 10.10 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.kt
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.kt
@@ -1,14 +1,3 @@
 class Solution {
-    fun solution(s: String): String {
-        var sList = s.toMutableList()
-        var answer = ""
-
-        if (s.length % 2 == 0) {
-            answer = sList[(s.length / 2) - 1].toString() + sList[s.length / 2].toString()
-        }
-        else {
-            answer = sList[s.length / 2].toString()
-        }
-        return answer
-    }
+    fun solution(s: String) = s.slice(((s.length - 1) / 2)..(s.length / 2))
 }
